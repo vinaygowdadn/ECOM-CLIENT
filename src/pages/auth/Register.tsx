964a0aa15c005f870c73f42bd8e377fafb67e906
vinaygowdadn <vinaydn1117@gmail.com>
2025-09-19 12:03:28 +0530

    update

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -2,23 +2,33 @@ import { Button } from "@/components/ui/button"
 import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@radix-ui/react-label"
+import { Link } from "react-router-dom"
 
 const Register = () => {
     return (
         <div className="w-svw h-[90vh] flex justify-center items-center">
             <Card className="w-full max-w-sm">
                 <CardHeader>
-                    <CardTitle>Login to your account</CardTitle>
+                    <CardTitle>Register your account</CardTitle>
                     <CardDescription>
-                        Enter your email below to login to your account
+                        Enter your details below to register to your account
                     </CardDescription>
                     <CardAction>
-                        <Button variant="link">Sign Up</Button>
+                        <Link to='/'>Login</Link>
                     </CardAction>
                 </CardHeader>
                 <CardContent>
                     <form>
                         <div className="flex flex-col gap-6">
+                            <div className="grid gap-2">
+                                <Label htmlFor="name">Name</Label>
+                                <Input
+                                    id="name"
+                                    type="text"
+                                    placeholder="vinay"
+                                    required
+                                />
+                            </div>
                             <div className="grid gap-2">
                                 <Label htmlFor="email">Email</Label>
                                 <Input
@@ -31,25 +41,26 @@ const Register = () => {
                             <div className="grid gap-2">
                                 <div className="flex items-center">
                                     <Label htmlFor="password">Password</Label>
-                                    <a
-                                        href="#"
-                                        className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                                    >
-                                        Forgot your password?
-                                    </a>
                                 </div>
-                                <Input id="password" type="password" required />
+                                <Input id="password" type="password" placeholder="******" required />
+                            </div>
+                            <div className="grid gap-2">
+                                <Label htmlFor="address">Address</Label>
+                                <Input
+                                    id="adress"
+                                    type="text"
+                                    placeholder="Mysuru"
+                                    required
+                                />
                             </div>
                         </div>
                     </form>
                 </CardContent>
                 <CardFooter className="flex-col gap-2">
                     <Button type="submit" className="w-full">
-                        Login
+                        Signup
                     </Button>
-                    <Button variant="outline" className="w-full">
-                        Login with Google
-                    </Button>
+
                 </CardFooter>
             </Card>
         </div>
